9daca6d2b396c79a846ae5cc012f79d365657e89
Sebastian M. Castillo Hair <castillohair@gmail.com>
2014-03-06 23:38:39 -0600

    Firmware: demo now updates using hardware clock.
    Moved update instructions of demo to a hardware-based interruption based
    on timer 1 that runs every 1ms.

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -12,66 +12,30 @@
 
 #include "Tlc5941/Tlc5941.h"
 
-/*ISR(TIMER5_COMPA_vect) {
+// Initialize variables for demo
+uint16_t wave[10][10] =
+{{7, 121, 858, 2770, 4095, 2770, 858, 121, 7, 0},
+{5, 95, 731, 2552, 4079, 2984, 999, 153, 10, 0},
+{4, 75, 618, 2333, 4031, 3189, 1155, 191, 14, 0},
+{2, 58, 518, 2116, 3953, 3381, 1324, 237, 19, 0},
+{2, 44, 431, 1904, 3846, 3557, 1506, 292, 25, 1},
+{1, 34, 356, 1700, 3714, 3714, 1700, 356, 34, 1},
+{1, 25, 292, 1506, 3557, 3846, 1904, 431, 44, 2},
+{0, 19, 237, 1324, 3381, 3953, 2116, 518, 58, 2},
+{0, 14, 191, 1155, 3189, 4031, 2333, 618, 75, 4},
+{0, 10, 153, 999, 2984, 4079, 2552, 731, 95, 5}};
+uint8_t shift = 0;
+uint8_t wave_index=0;
+
+ISR(TIMER1_COMPA_vect) {
 	static uint16_t count = 0; // milliseconds
-	// Period
-	if (count == 500)
+	
+	Tlc5941_channel_t i = 0; // Tlc channel
+	
+	if (count == 0)
 	{
-		OdLaser_TurnOn();
-	}
-	else if (count == 1000)
-	{
-		OdLaser_TurnOff();
-	}
-	// Increment counter
-	count ++;
-	if (count == 1000)
-		count = 0;
-}*/
-
-int main(void) {
-	// Initialize variables for demo
-	uint16_t wave[10][10] = 
-	{{7, 121, 858, 2770, 4095, 2770, 858, 121, 7, 0},
-	 {5, 95, 731, 2552, 4079, 2984, 999, 153, 10, 0},
-	 {4, 75, 618, 2333, 4031, 3189, 1155, 191, 14, 0},
-	 {2, 58, 518, 2116, 3953, 3381, 1324, 237, 19, 0},
-	 {2, 44, 431, 1904, 3846, 3557, 1506, 292, 25, 1},
-	 {1, 34, 356, 1700, 3714, 3714, 1700, 356, 34, 1},
-	 {1, 25, 292, 1506, 3557, 3846, 1904, 431, 44, 2},
-	 {0, 19, 237, 1324, 3381, 3953, 2116, 518, 58, 2},
-	 {0, 14, 191, 1155, 3189, 4031, 2333, 618, 75, 4},
-	 {0, 10, 153, 999, 2984, 4079, 2552, 731, 95, 5}};
-	uint8_t shift = 0;
-	uint8_t wave_index=0;
-	
-	// Initialize pins for TLC
-	Tlc5941_Init();
-    
-    /*// Initialize Timer 5 to generate an interruption every millisecond
-	// Mode: CTC, WGM5 = 0b0100
-	TCCR5B = (1 << WGM52);
-	// Clock: no prescaling
-	TCCR5B |= (1 << CS50);
-	// Compare value: 0x3E7F
-	OCR5A = 0x3E7F;
-	// Enable compare interrupt
-	TIMSK5 = (1 << OCIE5A);*/
-	
-	// The following two lines are optional
-	Tlc5941_SetAllDC(63);
-	Tlc5941_ClockInDC();
-	
-	// Default all channels to off
-	Tlc5941_SetAllGS(0);
-	
-	// Enable Global Interrupts
-	sei();
-
-	Tlc5941_channel_t i = 0;
-	while(1) {
 		// Wait until we can modify gsData
-		while(Tlc5941_gsUpdateFlag); 
+		while(Tlc5941_gsUpdateFlag);
 		
 		// Set all grayscale values to zero
 		Tlc5941_SetAllGS(0);
@@ -89,9 +53,40 @@ int main(void) {
 		}
 		// Set update flag
 		Tlc5941_SetGSUpdateFlag();
-		
-		// Wait 10ms
-		_delay_ms(10);
+	}
+	
+	// Increment counter
+	count ++;
+	if (count == 10)
+		count = 0;
+}
+
+int main(void) {
+	
+	// Initialize pins for TLC
+	Tlc5941_Init();
+    
+    // Initialize Timer 5 to generate an interruption every millisecond
+	// Mode: CTC, WGM5 = 0b0100
+	TCCR1B = (1 << WGM12);
+	// Clock: no prescaling
+	TCCR1B |= (1 << CS10);
+	// Compare value: 0x3E7F
+	OCR1A = 0x3E7F;
+	// Enable compare interrupt
+	TIMSK1 = (1 << OCIE1A);
+	
+	// The following two lines are optional
+	Tlc5941_SetAllDC(63);
+	Tlc5941_ClockInDC();
+	
+	// Default all channels to off
+	Tlc5941_SetAllGS(0);
+	
+	// Enable Global Interrupts
+	sei();
+
+	while(1) {
 	}
 	return 0;
 }
